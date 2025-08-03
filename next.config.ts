b580d8c1a9c199b681f256ba55b3b0adaf4edd8a
DylanTse520 <dylan052099@gmail.com>
2025-08-02 19:56:47 -0700

    Enhance Next.js configuration: add Webpack aliasing for improved module resolution, allowing for cleaner imports of libraries, data, store, and components. Update TypeScript configuration by removing unused path mappings for better clarity.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@lib": path.resolve(__dirname, "app/_lib"),
+      "@data": path.resolve(__dirname, "app/_data"),
+      "@store": path.resolve(__dirname, "app/_store"),
+      "@components": path.resolve(__dirname, "app/_components"),
+    };
+    return config;
   },
 };
 
